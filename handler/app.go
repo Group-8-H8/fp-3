25bb8fdaa0c6825845e11e192ecfac14c69d35e0
Abilsabili <abilsabili50@gmail.com>
2023-05-21 01:30:01 +0700

    chore: add swaggo dependencies, config, and new endpoint for swagger

--- handler/app.go
+++ handler/app.go
@@ -5,11 +5,14 @@ import (
 	"os"
 
 	"github.com/Group-8-H8/fp-3/database"
+	_ "github.com/Group-8-H8/fp-3/docs"
 	"github.com/Group-8-H8/fp-3/handler/http_handler"
 	"github.com/Group-8-H8/fp-3/repository/category_repository/category_pg"
 	"github.com/Group-8-H8/fp-3/repository/task_repository/task_pg"
 	"github.com/Group-8-H8/fp-3/repository/user_repository/user_pg"
 	"github.com/Group-8-H8/fp-3/service"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 
 	"github.com/gin-gonic/gin"
 )
@@ -65,6 +68,8 @@ func StartApp() {
 		taskRoute.PATCH("/update-category/:taskId", taskHandler.UpdateTasksCategory)
 		taskRoute.DELETE("/:taskId", taskHandler.DeleteTask)
 	}
+
+	route.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	if PORT = os.Getenv("PORT"); PORT == "" {
 		PORT = "3000"
